58a8b9d00a4fe26519dbfe883114d8369bc4477d
DeclanFitzpatrick <declan.fitzpatrick@ucalgary.ca>
2025-04-08 13:46:42 -0600

    I think I fixed the graphs

--- Examples/Q2driver.py
+++ Examples/Q2driver.py
@@ -13,7 +13,7 @@ def main():
     plt.scatter(p, vp, marker = 'o')
     plt.xlabel("Density (g/cm³)")
     plt.ylabel("P-wave Velocity (m/s)")
-    plt.title("P-wave Velocity vs. Density")
+    plt.title("Raw Data")
     plt.savefig("figures/Raw_data.png")
     plt.show()
 
@@ -38,17 +38,32 @@ def main():
     plt.plot(p, log_n, label='Fitted line', color='black')
     plt.xlabel("Density (g/cm³)")
     plt.ylabel("ln(P-wave Velocity)")
-    plt.title("Linearized Fit: ln(Vp) vs. Density")
+    plt.title("Linearized Data with Regression")
     plt.legend()
     plt.grid(True)
     plt.text(0.20,0.60, f'$R^2$ = {rsq:8f}' , transform=plt.gca().transAxes,fontsize=12, bbox=dict(facecolor='white'))
-    plt.savefig("figures/Q2_lnvp_fit.png")
+    plt.savefig("figures/linearized_data_with_regression.png")
     plt.show()
 
     # Part c)
     # Recover model parameters
     V0 = np.exp(a[0])   # V₀ = e^(intercept)
     k = a[1]             # slope is k
+
+    model = V0 * np.exp(k * p)  # model prediction
+    sorted_indices = np.argsort(p)  # sort indices for plotting
+    sorted_p = p[sorted_indices]
+    sorted_model = model[sorted_indices]
+
+    plt.figure()
+    plt.plot(sorted_p, sorted_model)
+    plt.scatter(p, vp,color='green', s=15)
+    plt.xlabel("Rho (g/cm³)")
+    plt.ylabel("Vp (m/s)")
+    plt.title("Raw Data with Regression Line")
+    plt.text(0.20,0.60,f'R2: {rsq:.4f}   V_0: {V0:.4f}   k: {k:.4f}')
+    plt.savefig("figures/Raw_data_with_regression.png")
+    plt.show()
 
     print(f"\nRecovered model parameters:")
     print(f"V₀ (m/s): {V0:.2f}")
